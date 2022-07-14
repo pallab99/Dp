5f6d75addc74d5cb15a6e7ec41c60d1e5e9770d2
pallab99 <37453678+pallab99@users.noreply.github.com>
2022-07-14 23:45:32 +0600

    Add files via upload

---
+++ Min_Cost_Climbing_Stairs_Top_Down.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(int n,vector<int>&cost,vector<int>&dp)
+    {
+        if(n==0)
+            return cost[0];
+        if(n==1)
+            return cost[1];
+        
+        if(dp[n]!=-1)
+            return dp[n];
+        dp[n]=cost[n]+=min(solve(n-1,cost,dp),solve(n-2,cost,dp));
+        return dp[n];
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int>dp(n+1,-1);
+        int ans=min(solve(n-1,cost,dp),solve(n-2,cost,dp));
+        return ans;
+    }
+};
