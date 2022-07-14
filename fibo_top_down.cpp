659430a72234c44ee08ab1f9e8aa515532757586
pallab99 <37453678+pallab99@users.noreply.github.com>
2022-07-14 22:00:55 +0600

    Add files via upload

---
+++ fibo_top_down.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fib(int n,vector<int>&dp)
+{
+    if(n==1 or n==0)
+    return n;
+    if(dp[n]!=-1)
+    return dp[n];
+    dp[n]=fib(n-1,dp)+fib(n-2,dp);
+
+    return dp[n];
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>dp(n+1,-1);
+    cout<<fib(n,dp);
+}
